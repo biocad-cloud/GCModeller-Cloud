e29c3e1f3c64d4cbd017a840549b0bbafd9145a2
xieguigang <xie.guigang@gcmodeller.org>
2021-10-23 02:20:30 +0800

    Update docker.sh

---
+++ docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker stop biocad_web
+docker rm biocad_web
+
+docker run -itd --name biocad_web \
+    --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor=unconfined \
+	--privileged=true \
+	-v "$(which docker):/bin/docker" \
+	-v "/var/run/docker.sock:/run/docker.sock" \
+    -v "/tmp/biocad_tmp2:/tmp:rw" \
+	-v "/tmp/apache/:/tmp/apache/:rw" \
+	-v "/opt/biocad/src/web:/var/www/html/:r" \
+	-p 8848:80 \
+	-p 8843:443 \
+	web_env:php /usr/sbin/init
+
+docker exec -it biocad_web systemctl restart httpd
