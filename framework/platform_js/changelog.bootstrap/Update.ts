d8b95f540f5ad3195bb5817a27efc21c21fc5be4
xieguigang <xie.guigang@gcmodeller.org>
2021-11-14 10:59:39 +0800

    add typescript source

---
+++ framework/platform_js/changelog.bootstrap/Update.ts
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../../build/linq.d.ts" />
+
+class Update {
+
+    /**
+     * The github commit hash of this update
+    */
+    public commit_hash: string;
+    public date: Date;
+    public updates: updateLog[];
+
+    public get HTML(): string {
+        return `
+            <p>
+                <strong>UPDATE: ${this.date.toLocaleDateString()}</strong> 
+                        [<a href="https://github.com/GCModeller-Cloud/bioCAD/tree/${this.commit_hash}">${this.commit_hash}</a>]
+            </p>
+            <p>
+                ${From(this.updates).Select(i => i.bootstrapItem).JoinBy("<br />\n")}
+            </p>`;
+    }
+}
+
