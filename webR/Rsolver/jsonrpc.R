ccd334b76816551abcd78eecc3ee1eb6f1719f2f
xieguigang <xie.guigang@gcmodeller.org>
2021-12-12 03:36:25 +0800

    usreu for the web calls

--- webR/Rsolver/jsonrpc.R
+++ webR/Rsolver/jsonrpc.R
@@ -1,8 +1,16 @@
 require(JSON);
+require(http);
 
 #' jsonrpc to biocad services
 #' 
 call_rpc = function(func, args) {
     endpoint = "http://8.210.29.117:8848/biostack/jsonrpc/";
+    payload = list(
+        jsonrpc = "2.0",
+        method = func,
+        params = args,
+        id = 0
+    );
+
 
 }
