2061a4fe765896b8fdf0bee045e5125e8c86e196
xieguigang <xie.guigang@gcmodeller.org>
2021-12-11 21:35:29 +0800

    Update background.php

--- webR/background.php
+++ webR/background.php
@@ -1,2 +1,52 @@
 <?php
 
+/**
+ * application root dir
+*/
+define("APP_PATH", dirname(__DIR__));
+
+# run the background task script based on the app_id
+require APP_PATH . "/framework/php.NET/package.php";
+
+imports("Microsoft.VisualBasic.CommandLine.CommandLineParser");
+imports("Microsoft.VisualBasic.FileIO.FileSystem");
+imports("php.docker/*");
+
+# php ./taskhost.php --app_id=xxx --interval=1 --signal="signal.txt"
+$args    = CommandLineParser::ParseCLIArgvs();
+$app_id  = $args->getArgumentValue("app_id", NULL);
+$taskMgr = new Table("task");
+$filter  = ["status" => 0];
+
+# check for status = 0 // pending status
+if (!Utils::isDbNull($app_id)) {
+    $filter["app_id"] = $app_id;
+}
+
+$pending = $taskMgr
+    ->left_join("analysis_app")
+    ->on([
+        "analysis_app" => "id",
+        "task" => "app_id"
+    ])
+    ->where($filter)
+    ->find();
+
+if (Utils::isDbNull($pending)) {
+    console::log("~job done!");
+    exit(0);
+}
+
+$R = $pending["app_path"];
+$args = $pending["parameters"];
+$commandline = "Rscript $R --args $args --guid {$pending["sha1"]}";
+$volumn = [
+    "Rscript" => ["host" => dirname($R), "virtual" => dirname($R)],
+    "mnt"     => ["host" => "/mnt", "virtual" => "/mnt"],
+    "tmp"     => ["host" => "/tmp", "virtual" => "/tmp"]
+];
+
+# task status will be updated in rscript.
+docker::run("dotnet::gcmodeller_v5.23.2", $commandline, "/mnt", $volumn);
+
+console::log("~job done!");
