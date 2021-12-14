301254ea56d50a7b8facdb77e3a66e39c0e38086
xieguigang <xie.guigang@gcmodeller.org>
2021-12-14 22:54:06 +0800

    view model data

--- webR/Rsolver/PLAS.R
+++ webR/Rsolver/PLAS.R
@@ -19,10 +19,27 @@ file = `${model$uri}/${model$current_version}.${model$suffix}`;
 print("get model file path:");
 print(file);
 
+const cast_table as function(list) {
+    allFields = unique(unlist(lapply(list, names)));
+    data = data.frame(id = 1:length(list));
+
+    for(name in allFields) {
+        data[, name] = sapply(list, r -> r[[name]]);
+    }
+
+    data;
+}
+
 const to_Ssystem as function(model) {
     print("view of the model file content:");
-    str(model);
-    
+    # str(model);
+
+    nodes = model$nodeDataArray;
+    links = model$linkDataArray;
+
+    print(cast_table(nodes));
+    print(cast_table(links));
+
 }
 
 model = file 
