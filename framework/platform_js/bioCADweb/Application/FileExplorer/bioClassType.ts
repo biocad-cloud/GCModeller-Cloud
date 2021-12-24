3a0d1b56bdbba2a4bbdedf3d8160c2a795b545b7
xieguigang <xie.guigang@gcmodeller.org>
2021-12-25 02:58:57 +0800

    move to application folder

---
+++ framework/platform_js/bioCADweb/Application/FileExplorer/bioClassType.ts
@@ -0,0 +1,22 @@
+﻿enum bioClassType {
+    /**
+     * The unknown class type
+    */
+    unknown = 0,
+    /**
+     * General text file
+    */
+    text = 1,
+    /**
+     * Image file
+    */
+    image = 2,
+    /**
+     * The data table is a kind of numeric matrix for gene expression data, or something.
+    */
+    matrix = 3,
+    /**
+     * The biological sequence data type, like fasta sequence file.
+    */
+    bioSequence = 4
+}
