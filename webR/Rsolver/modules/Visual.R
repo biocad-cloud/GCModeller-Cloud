63cc43473b4b9014b0d16e43b442ec5250644754
xieguigang <xie.guigang@gcmodeller.org>
2021-12-26 01:23:14 +0800

    print data

--- webR/Rsolver/modules/Visual.R
+++ webR/Rsolver/modules/Visual.R
@@ -4,4 +4,13 @@
 FluxVisual = function(fluxomics, sample_info, background, outputdir = "./") {
     print("view of the pathway collection group:");
     str(background);
+
+    print("plot flux lines for each pathway group:");
+
+    for(pathway in names(background)) {
+        nodes = background[[pathway]];
+
+        print(pathway);
+        print(nodes, max.print = 10);
+    }
 }
