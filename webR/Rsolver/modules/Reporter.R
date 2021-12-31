1a9dfa61bdc8d87f6ae0d95199b4b6bf603e8e9b
xieguigang <xie.guigang@gcmodeller.org>
2022-01-01 03:25:51 +0800

    create reports

--- webR/Rsolver/modules/Reporter.R
+++ webR/Rsolver/modules/Reporter.R
@@ -6,6 +6,32 @@ require(JSON);
 Report = function(model, workdir) {
     report = reportTemplate(`${@dir}/../reports/PLAS_Dynamics`);
     report["plas_graph"] = model;
+    report["pathway_dynamics"] = pathway_dynamics(workdir);
+    report["flux_dynamics"]    = flux_dynamics(workdir);
 
     pdf::makePDF(report, pdfout = `${workdir}/report.pdf`);
+}
+
+pathway_dynamics = function(workdir) {
+    `${workdir}/visual/pathway/`
+    |> list.files(pattern = "*.png")
+    |> sapply(function(file) {
+        img = dataUri(file);
+        img = `<img src="${img}" style="width: 90%;"/>`;
+        img; 
+    })
+    |> paste("")
+    ;
+}
+
+flux_dynamics = function(workdir) {
+    `${workdir}/visual/flux/`
+    |> list.files(pattern = "*.png")
+    |> sapply(function(file) {
+        img = dataUri(file);
+        img = `<img src="${img}" style="width: 90%;"/>`;
+        img; 
+    })
+    |> paste("")
+    ;
 }
