6ca92ab052364f3f09f7b0aa8764359648779f8b
xieguigang <xie.guigang@gcmodeller.org>
2022-01-02 02:04:56 +0800

    render pathway graph

--- webR/Rsolver/modules/Reporter.R
+++ webR/Rsolver/modules/Reporter.R
@@ -1,11 +1,13 @@
 require(Markdown2Pdf);
 require(JSON);
+require(igraph);
+require(ggplot);
 
 #' Create html and pdf report
 #' 
 Report = function(model, workdir) {
     report = reportTemplate(`${@dir}/../reports/PLAS_Dynamics`);
-    report["plas_graph"] = model;
+    report["plas_graph"] = render_graph(JSON::json_decode(model), workdir);
     report["WEBCONTEXT"] = "http://biocad.cloud:8848/";
     report["pathway_dynamics"] = pathway_dynamics(workdir);
     report["flux_dynamics"]    = flux_dynamics(workdir);
@@ -18,6 +20,42 @@ Report = function(model, workdir) {
             loaderrorhandling = "ignore"
         )
     );
+}
+
+render_graph = function(json, workdir) {
+    nodes = as.data.frame(json$nodeDataArray);
+    nodes = nodes[(nodes[, "category"] != "valve"), ];
+
+    links = as.data.frame(json$linkDataArray);
+    groupNames = nodes[as.logical(nodes[, "isGroup"]), ];
+    groupNames = as.list(groupNames, byrow = TRUE);
+    groupNames = lapply(groupNames, r -> r$text, names = r -> r$key);
+
+    str(groupNames);
+    print(nodes, max.print = 13);
+
+    g = graph(from = links[, "from"], to = links[, "to"]);
+    v = V(g);
+
+    print(xref(v));
+
+    xref = xref(v);
+
+    i = sapply(nodes[, "key"], id -> which(id == xref));
+    print(i);
+    v$label = (nodes[, "label"])[i];
+    v$group = sapply((nodes[, "group"])[i], key ->  ifelse((!is.null(key)) && (key in groupNames), groupNames[[key]], "undefined"));
+
+    print(v$label);
+    print(v$group);
+
+    bitmap(file = `${workdir}/graph.png`, size = [3200, 2700]) {
+        ggplot(g) + geom_edge_link() + geom_node_point() + geom_node_text() + layout_forcedirected();
+    }
+
+    graph = dataUri(`${workdir}/graph.png`);
+    graph = `<img src="${graph}" style="width: 100%;"/>`;
+    graph; 
 }
 
 cover_image = function() {
