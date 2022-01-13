64c202596d710a8deae5ce7691ff1e43f12926f1
xieguigang <xie.guigang@gcmodeller.org>
2022-01-13 21:52:46 +0800

    Update start.sh

--- webR/wwwroot/start.sh
+++ webR/wwwroot/start.sh
@@ -5,5 +5,5 @@ while true
 do
 	echo "Press CTRL+C to stop the script execution"
 	
-	docker run -it -v "$PWD:/opt/biocad/" -p 8847:80 dotnet:gcmodeller_v5.23.2 Rserve --start --port 7452 --Rweb "/opt/biocad/" --show_error --n_threads 8
+	docker run -it -v "$PWD:/opt/biocad/" -w "/opt/biocad/" -p 8847:80 dotnet:gcmodeller_v5.23.2 Rserve --start --port 80 --Rweb "/opt/biocad/" --show_error --n_threads 2
 done
