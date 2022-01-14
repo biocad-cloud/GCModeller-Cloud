37d76235823410d5ae628a24fa96a75511cf0537
xieguigang <xie.guigang@gcmodeller.org>
2022-01-14 20:49:04 +0800

    Update KeggMap.R

--- webR/wwwroot/KeggMap.R
+++ webR/wwwroot/KeggMap.R
@@ -1,7 +1,7 @@
 require(GCModeller);
-
+print("~~~~~~~~~~~");
 imports "report.utils" from "kegg_kit";
-
+print("==================");
 const run as function(id) {
 	const kegg = GCModeller::kegg_maps(rawMaps = FALSE);
 	const map  = kegg[[id]]; 
