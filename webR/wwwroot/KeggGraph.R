df241df47ac9ab9761a581d6e1c5fd19dcca505b
xieguigang <xie.guigang@gcmodeller.org>
2022-01-15 18:32:36 +0800

    Update KeggGraph.R

--- webR/wwwroot/KeggGraph.R
+++ webR/wwwroot/KeggGraph.R
@@ -5,9 +5,11 @@ require(JSON);
 # map00020
 const run as function(id) {
 	const kegg = GCModeller::kegg_maps(rawMaps = FALSE);
-	const map  = kegg[[id]]; 
-	const cid  = compoundsId(map);
-	const g    = fromCompounds(cid, GCModeller::kegg_reactions());
+	const map  = kegg[[id]]; 	
+	const g    = map 
+	|> compoundsId() 
+	|> fromCompounds(GCModeller::kegg_reactions(), enzymeBridged = FALSE)
+	;
 	
 	editor_graph(V(g), E(g))	
 	|> json_encode()
@@ -16,8 +18,8 @@ const run as function(id) {
 }
 
 const editor_graph as function(v, e) {
-	const nodes = sapply(v, x -> list(key = x$label,category = "stock",label = x$text,loc = "0,0",group = NULL));
-	const links = sapply(e, l -> list(category = "flow",text = l$ID,from = as.object(l$U)$label,to = as.object(l$V)$label,labelKeys = []));
+	const nodes = sapply(v, x -> list(key = x$label, category = "stock", label = x$text, loc = "0,0", group = NULL));
+	const links = sapply(e, l -> list(category = "flow", text = l$ID, from = as.object(l$U)$label, to = as.object(l$V)$label, labelKeys = []));
 
 	list(
 		class = "GraphLinksModel",
