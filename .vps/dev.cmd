b28f3d599427effde987d873d11c987525834489
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-25 23:17:44 +0800

    not working

--- .vps/dev.cmd
+++ .vps/dev.cmd
@@ -25,20 +25,18 @@ echo "init docker container for the httpd web..."
 
 %{{%
 
-    docker run -itd --name biocad_web --cap-add SYS_ADMIN --privileged=true 
-
-    -v "%dev_tmp%/biocad_tmp2:/tmp:rw" 
-    -v "%dev_tmp%/apache:/tmp/apache/:rw" 
-    -v "%proj_root%/framework/php.NET/:/opt/runtime/:ro" 
-    -v "%proj_root%/framework/MaxMind/:/opt/vendor/MaxMind/:ro" 
-    -v "%proj_root%/repository/:/opt/biocad/biocad_registry/:ro" 
-    -v "%proj_root%/:/opt/biocad/biocad_cloud/:ro" 
-    -v "%proj_root%/src/web/resources/fonts/:/opt/biocad/biocad_cloud/web/resources/styles/fonts/:ro" 
+    docker run -itd --name biocad_web  
+    -v "%dev_tmp%/biocad_tmp2:/tmp" 
+    -v "%dev_tmp%/apache:/tmp/apache/" 
+    -v "%proj_root%/framework/php.NET/:/opt/runtime/" 
+    -v "%proj_root%/framework/MaxMind/:/opt/vendor/MaxMind/" 
+    -v "%proj_root%/repository/:/opt/biocad/biocad_registry/" 
+    -v "%proj_root%/:/opt/biocad/biocad_cloud/" 
+    -v "%proj_root%/src/web/resources/fonts/:/opt/biocad/biocad_cloud/web/resources/styles/fonts/" 
     -v "%proj_root%/.vps/vhost:/etc/httpd/vhost" 
-    -v "%proj_root%/.vps/mnt:/mnt/biocad_cloud:rw" 
+    -v "%proj_root%/.vps/mnt:/mnt/biocad_cloud" 
     -p 8848:80 
     -p 8843:443 
-    
     ubuntu:php8 /usr/sbin/init
 
 %}}%
