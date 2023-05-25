77771019a3b2a65a166234ea428d36a0e2db986e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-25 23:11:28 +0800

    Update dev.cmd

--- .vps/dev.cmd
+++ .vps/dev.cmd
@@ -1,4 +1,7 @@
 @echo off
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set "{{=setlocal enableDelayedExpansion&for %%a in (" & set "}}="::end::" ) do if "%%~a" neq "::end::" (set command=!command! %%a) else (call !command! & endlocal)"
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 docker stop biocad_web
 docker rm biocad_web
@@ -13,29 +16,32 @@ echo %dev_tmp%
 
 echo "start to create the dev folders:"
 
-mkdir %dev_tmp%
-mkdir %dev_tmp%/biocad_tmp2
-mkdir %dev_tmp%/apache
-
-mkdir %proj_root%/.vps/mnt
+mkdir "%dev_tmp%"
+mkdir "%dev_tmp%/biocad_tmp2"
+mkdir "%dev_tmp%/apache"
+mkdir "%proj_root%/.vps/mnt"
 
 echo "init docker container for the httpd web..."
 
-docker run -itd --name biocad_web ^
-    --cap-add SYS_ADMIN --security-opt apparmor=unconfined ^
-    --privileged=true ^    
-    -v "%dev_tmp%/biocad_tmp2:/tmp:rw" ^
-    -v "%dev_tmp%/apache:/tmp/apache/:rw" ^
-    -v "%proj_root%/framework/php.NET/:/opt/runtime/:ro" ^
-    -v "%proj_root%/framework/MaxMind/:/opt/vendor/MaxMind/:ro" ^
-    -v "%proj_root%/repository/:/opt/biocad/biocad_registry/:ro" ^
-    -v "%proj_root%/:/opt/biocad/biocad_cloud/:ro" ^
-    -v "%proj_root%/src/web/resources/fonts/:/opt/biocad/biocad_cloud/web/resources/styles/fonts/:ro" ^
-    -v "%proj_root%/.vps/vhost:/etc/httpd/vhost" ^
-    -v "%proj_root%/.vps/mnt:/mnt/biocad_cloud:rw" ^
-    -p 8848:80 ^
-    -p 8843:443 ^
+%{{%
+
+docker run -itd --name biocad_web 
+    --cap-add SYS_ADMIN --security-opt apparmor=unconfined 
+    --privileged=true 
+    -v "%dev_tmp%/biocad_tmp2:/tmp:rw" 
+    -v "%dev_tmp%/apache:/tmp/apache/:rw" 
+    -v "%proj_root%/framework/php.NET/:/opt/runtime/:ro" 
+    -v "%proj_root%/framework/MaxMind/:/opt/vendor/MaxMind/:ro" 
+    -v "%proj_root%/repository/:/opt/biocad/biocad_registry/:ro" 
+    -v "%proj_root%/:/opt/biocad/biocad_cloud/:ro" 
+    -v "%proj_root%/src/web/resources/fonts/:/opt/biocad/biocad_cloud/web/resources/styles/fonts/:ro" 
+    -v "%proj_root%/.vps/vhost:/etc/httpd/vhost" 
+    -v "%proj_root%/.vps/mnt:/mnt/biocad_cloud:rw" 
+    -p 8848:80 
+    -p 8843:443 
     ubuntu:php8 /usr/sbin/init
+
+%}}%
 
 echo "start target web server!"
 
