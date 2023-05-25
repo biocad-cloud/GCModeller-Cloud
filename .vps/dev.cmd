24430a2f57eadf4276dc43f7b7e17b05a75d7c8c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-25 23:04:42 +0800

    fix for the command batch

--- .vps/dev.cmd
+++ .vps/dev.cmd
@@ -10,24 +10,24 @@ mkdir %dev_tmp%
 mkdir %dev_tmp%/biocad_tmp2
 mkdir %dev_tmp%/apache
 
-mkdir 
+mkdir %proj_root%/.vps/mnt
 
 docker run -itd --name biocad_web ^
     --cap-add SYS_ADMIN --security-opt apparmor=unconfined ^
     --privileged=true ^    
-    -v "%dev_tmp%/biocad_tmp2:/tmp:rw" \
-    -v "%dev_tmp%/apache:/tmp/apache/:rw" \
-    -v "%proj_root%/framework/php.NET/:/opt/runtime/:ro" \
-    -v "%proj_root%/framework/MaxMind/:/opt/vendor/MaxMind/:ro" \
-    -v "%proj_root%/repository/:/opt/biocad/biocad_registry/:ro" \
-    -v "%proj_root%/:/opt/biocad/biocad_cloud/:ro" \
-    -v "%proj_root%/src/web/resources/fonts/:/opt/biocad/biocad_cloud/web/resources/styles/fonts/:ro" \
-    -v "%proj_root%/.vps/vhost:/etc/httpd/vhost" \
-    -v "%proj_root%/.vps/mnt:/mnt/biocad_cloud:rw" \
-    -p 8848:80 \
-    -p 8843:443 \
-    web_env:php /usr/sbin/init
+    -v "%dev_tmp%/biocad_tmp2:/tmp:rw" ^
+    -v "%dev_tmp%/apache:/tmp/apache/:rw" ^
+    -v "%proj_root%/framework/php.NET/:/opt/runtime/:ro" ^
+    -v "%proj_root%/framework/MaxMind/:/opt/vendor/MaxMind/:ro" ^
+    -v "%proj_root%/repository/:/opt/biocad/biocad_registry/:ro" ^
+    -v "%proj_root%/:/opt/biocad/biocad_cloud/:ro" ^
+    -v "%proj_root%/src/web/resources/fonts/:/opt/biocad/biocad_cloud/web/resources/styles/fonts/:ro" ^
+    -v "%proj_root%/.vps/vhost:/etc/httpd/vhost" ^
+    -v "%proj_root%/.vps/mnt:/mnt/biocad_cloud:rw" ^
+    -p 8848:80 ^
+    -p 8843:443 ^
+    ubuntu:php8 /usr/sbin/init
 
-docker exec -it biocad_web systemctl restart httpd
+docker exec -it biocad_web systemctl restart apache2
 
 pause
