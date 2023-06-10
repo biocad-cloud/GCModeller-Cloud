10f38fa1e6528bf090bc2fc18ffcc52aba0df4c8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-10 11:43:22 +0800

    Update build.cmd

--- mysql/build.cmd
+++ mysql/build.cmd
@@ -1,4 +1,10 @@
 @echo off
 
-reflector --reflects /sql bioCAD.sql -o ./VisualBasic /namespace MySql.bioCAD --language visualbasic /split /auto_increment.disable
-reflector /MySQL.Markdown /sql ./bioCAD.sql > ./README.md
+set reflector="F:\graphQL\src\mysqli\App\Reflector.exe"
+set R_src="F:\biocad_registry\src\mysql"
+set R_docs=F:\biocad_registry\src
+
+%reflector% --reflects /sql biocad_registry.sql -o %R_src% /namespace biocad_registry --language visualbasic /split /auto_increment.disable
+%reflector% /MySQL.Markdown /sql ./biocad_registry.sql > %R_docs%/README.md
+
+pause
