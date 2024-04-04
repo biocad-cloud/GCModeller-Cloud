2005854f1d601d85c584889943b5a20c40037a34
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-04 17:08:05 +0800

    config scripts

--- mysql/build.cmd
+++ mysql/build.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-set reflector="F:\graphQL\src\mysqli\App\Reflector.exe"
-set R_src="F:\biocad_registry\src\mysql"
-set R_docs=F:\biocad_registry\src
+set reflector="\graphQL\src\mysqli\App\Reflector.exe"
+set R_src="\biocad_registry\src\mysql"
+set R_docs=\biocad_registry\src
 
-%reflector% --reflects /sql biocad_registry.sql -o %R_src% /namespace biocad_registry --language visualbasic /split /auto_increment.disable
+%reflector% --reflects /sql biocad_registry.sql -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
 %reflector% /MySQL.Markdown /sql ./biocad_registry.sql > %R_docs%/README.md
 
 pause
