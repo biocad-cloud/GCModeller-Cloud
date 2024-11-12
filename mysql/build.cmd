c2ac22b9bfa2de84867ed0b47c6fccf80a6499ae
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-12 18:45:37 +0800

    Update build.cmd

--- mysql/build.cmd
+++ mysql/build.cmd
@@ -3,8 +3,10 @@ REM @echo off
 set reflector="\graphQL\src\mysqli\App\Reflector.exe"
 set R_src="\biocad_registry\src\mysql"
 set R_docs=\biocad_registry\src
+set render_exports="\cell-render\src\CellRender\registry"
 
 %reflector% --reflects /sql biocad_registry.sql -o %R_src% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
+%reflector% --reflects /sql biocad_registry.sql -o %render_exports% /namespace biocad_registryModel --language visualbasic /split /auto_increment.disable
 %reflector% /MySQL.Markdown /sql ./biocad_registry.sql > %R_docs%/README.md
 %reflector% --compares /current ./biocad_registry_current.sql /updates ./biocad_registry.sql /output ./biocad_registry_updates.md
 
