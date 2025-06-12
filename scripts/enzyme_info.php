6de554370c200989263677c0c3612697b04050ab
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-12 15:13:49 +0800

    show proteins

--- scripts/enzyme_info.php
+++ scripts/enzyme_info.php
@@ -9,11 +9,20 @@ class enzyme_info {
             ->where(["term" => $ec])
             ->select("`reaction`.*")
             ;
-
+        $prots = (new Table(["cad_registry"=>"db_xrefs"]))
+            ->left_join("molecule")
+            ->on(["molecule"=>"id","db_xrefs"=>"obj_id"])
+            ->left_join("ncbi_taxonomy")
+            ->on(["ncbi_taxonomy"=>"id","molecule"=>"tax_id"])
+            ->where(["db_key"=>354,"xref"=>$ec])
+            ->select(["`molecule`.id","name", "tax_id", "note", "taxname"])
+            ;
+    
         return [
             "reaction" => $rxns,
             "ec_id" => $ec,
-            "note" => ""
+            "note" => "",
+            "protein" => $prots
         ];
     }
 }
