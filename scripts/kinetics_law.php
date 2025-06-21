a838b2127c9f9a4bb67acd152b66f688fb56b2d3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-06-21 22:17:05 +0800

    Update kinetics_law.php

--- scripts/kinetics_law.php
+++ scripts/kinetics_law.php
@@ -4,12 +4,12 @@ class kinetics_law {
 
     public static function data($id) {
         $law_data = (new Table(["cad_registry"=>"kinetic_law"]))->where(["id"=>$id])->find();
-        $law_data["Params"] = json_decode($law_data["Params"]);
-        $law_data["Json_str"] = json_decode($law_data["Json_str"]);
+        $law_data["params"] = json_decode($law_data["params"], true);
+        $law_data["json_str"] = json_decode($law_data["json_str"], true);
         $law_data["reaction"] = (new Table(["cad_registry"=>"regulation_graph"]))
             ->left_join("reaction")
             ->on(["reaction"=>"id","regulation_graph"=>"reaction_id"])
-            ->where(["term"=>$law_data["Ec_number"]])
+            ->where(["term"=>$law_data["ec_number"]])
             ->select("`reaction`.*")
             ;
 
