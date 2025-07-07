1f8055b2ad6c8c176e9652940a8cfb4d3ec403ec
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-07-07 17:09:03 +0800

    tweaks of the page ui

--- scripts/index.php
+++ scripts/index.php
@@ -5,9 +5,11 @@ class index_page {
     public static function index_info() {
         # $mols = (new Table(["cad_registry"=>"molecule"]))->count();
         $mols = "&gt; 1M ";
+        $laws = "&gt; 35K";
 
         return [
-            "mols"=>$mols
+            "mols"=>$mols,
+            "laws"=>$laws
         ];
     }
 }
