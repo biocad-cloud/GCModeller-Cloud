69c665577393fd8a6904b4fbd04b1fa50b49e780
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-02 11:03:38 +0800

    query by id

--- scripts/search.php
+++ scripts/search.php
@@ -2,25 +2,52 @@
 
 class search_tool {
 
+    private static function text_sql($q) {
+        return "SELECT 
+            molecule.id,
+            name,
+            ROUND(mass, 4) AS mass,
+            formula,
+            molecule.note,
+            term AS type,
+            MATCH (name , molecule.note) AGAINST ('$q' IN BOOLEAN MODE) as score
+        FROM
+            cad_registry.molecule
+                LEFT JOIN
+            vocabulary ON vocabulary.id = type
+        WHERE
+            MATCH (name , molecule.note) AGAINST ('$q' IN BOOLEAN MODE)
+        order by score desc
+        limit 100";
+    }
+
+    private static function xref_sql($q) {
+        return "SELECT 
+            molecule.id,
+            name,
+            ROUND(mass, 4) AS mass,
+            formula,
+            molecule.note,
+            term AS type,
+            1000 AS score
+        FROM
+            cad_registry.molecule
+                LEFT JOIN
+            vocabulary ON vocabulary.id = type
+        WHERE
+            molecule.id IN (SELECT 
+                    obj_id
+                FROM
+                    db_xrefs
+                WHERE
+                    xref = '$q')";
+    }
+
     public static function get_result($q) {
         $q = str_replace("'", "", $q);
-        $sql = "SELECT 
-                molecule.id,
-                name,
-                ROUND(mass, 4) AS mass,
-                formula,
-                molecule.note,
-                term AS type,
-                MATCH (name , molecule.note) AGAINST ('$q' IN BOOLEAN MODE) as score
-            FROM
-                cad_registry.molecule
-                    LEFT JOIN
-                vocabulary ON vocabulary.id = type
-            WHERE
-                MATCH (name , molecule.note) AGAINST ('$q' IN BOOLEAN MODE)
-            order by score desc
-            limit 100"
-        ;
+        $q1 = self::text_sql($q);
+        $q2 = self::xref_sql($q);
+        $sql = "SELECT * FROM (($q1) UNION ($q2)) t1 ORDER BY score DESC";
         $mols = (new Table(["cad_registry"=>"molecule"]))->exec($sql, true);
         $sql = "SELECT 
                 *
