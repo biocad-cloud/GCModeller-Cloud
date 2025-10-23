3880858c0ddb9f8c6c049b9c19a078b00d94f169
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-23 22:54:11 +0800

    Create registry.php

---
+++ src/registry.php
@@ -0,0 +1,13 @@
+<?php
+
+include dirname(__DIR__) . "/etc/bootstrap.php";
+
+class App {
+
+    /**
+     * query reaction network by a given ec number
+    */
+    public function reaction_network($ec_number) {
+
+    }
+}
