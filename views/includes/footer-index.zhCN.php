f5630a0c9d37663e91e09cb75eb1238ae3e33d58
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-23 17:29:49 +0800

    update titles

--- views/includes/footer-index.zhCN.php
+++ views/includes/footer-index.zhCN.php
@@ -1,7 +1,13 @@
 <?php
 
 return [
-    "details_loc" => "秧塘工业园区",
-    "city_loc" => "广西 桂林",
-    "china" => "中国"
+    "details_loc" => "秧塘工业园区 华为信息产业园",
+    "city_loc" => "桂林 西城经济开发区",
+    "china" => "中国",
+    "svr1" => "虚拟细胞模型设计",
+    "svr2" => "系统生物数据库服务",
+    "svr3" => "发酵方案设计",
+    "our_services" => "我们的科研服务业务",
+    "contact_us" => "联系我们",
+    "links" => "友情链接"
 ];
