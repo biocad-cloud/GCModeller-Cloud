a80c4d5a3ddddf7c2471be6a8348bc8289741db7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-18 00:02:39 +0800

    make template blank page

--- src/index.php
+++ src/index.php
@@ -13,5 +13,14 @@ class App {
     public function index() {       
         View::Display();
     }
-
+    
+    /**
+     * Cella LAB
+     * 
+     * @access *
+     * @uses view
+    */
+    public function experiments() {
+        View::Display();
+    }
 }
